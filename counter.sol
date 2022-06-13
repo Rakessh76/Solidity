ad235e3531725add3fd0f02f45a05da2cd08d3e3
Rakessh76 <106570918+Rakessh76@users.noreply.github.com>
2022-06-13 18:56:55 +0530

    Create counter.sol

---
+++ counter.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.8.0;
+contract counter{
+    int public count;
+    function inc() external returns(int){
+        return count+=1; 
+    }
+    function dec() external returns(int){
+        return count-=1;
+    }
+}
